612583f44e8375db4aa0b4075e29b5865beb59a1
Kalyani <kanakalakalyani123@gmail.com>
2024-06-02 20:56:59 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}","./index.html","./src/input.css"],
+  theme: {
+    fontFamily:{
+        monstserrat:['Montserrat','sans-serif'],
+        inter:['Inter','sans-serif'],
+        primary:['Open Sans','sans-serif']
+    },
+    extend: {
+      colors:{
+      'primary':'#0872BF',
+      'background':"#141A1A"
+
+    },
+  },
+  },
+  plugins: [],
+}
+
